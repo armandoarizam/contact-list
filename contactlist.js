5979b253911fdbfd0a2a16f5b98b16ba3da31b7f
armandoarizam <armandoarizamartinez@gmail.com>
2023-06-27 12:24:49 -0500

    project-1

---
+++ contactlist.js
@@ -0,0 +1,33 @@
+let contactList = [
+    "Armando Ariza",
+    "Gabriela Delfin",
+    "Miguel Vergara",
+]
+
+function addContact (nuevo){
+    contactList.push(nuevo);
+}
+
+addContact("junior Ariza");
+      
+
+function deleteContact (){
+    contactList.pop();
+}
+
+deleteContact();
+
+function printContact (){
+    console.log(contactList)
+    
+}
+
+printContact();
+    
+
+    
+
+    /*console.log(contactList);*/ 
+
+
+
